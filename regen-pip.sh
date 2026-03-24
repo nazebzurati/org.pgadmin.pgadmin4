182756fdb9aaf9e8962aef7550bf69bc38bd5dfa
Nazeb Zurati <nazeb04@gmail.com>
2026-03-24 15:29:24 +0800

    Fix requirements file URL

--- regen-pip.sh
+++ regen-pip.sh
@@ -1,5 +1,5 @@
 # Increase this on each version update and rerun the script
-wget https://raw.githubusercontent.com/pgadmin-org/pgadmin4/REL-9_12/requirements.txt
+wget https://raw.githubusercontent.com/pgadmin-org/pgadmin4/REL-9_13/requirements.txt
 
 # https://github.com/flatpak/flatpak-builder-tools/issues/365
 cat requirements.txt | grep -v "<= '3.9'" | grep -v "< '3.10'" | grep -v sys_platform==\"win32\" > requirements_filtered.txt
